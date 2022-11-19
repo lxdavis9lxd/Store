39106f5694e233c3fcf46e751269d637b1858ce6
lxdavis9lxd <lxdavis9lxd@gmail.com>
2022-11-18 22:23:42 -0600

    revirese changes

--- NodeJSAPI/middleware/jwtVerify.js
+++ NodeJSAPI/middleware/jwtVerify.js
@@ -9,7 +9,7 @@ module.exports = function (req, res, next) {
             return res.status(StatusCodes.UNAUTHORIZED).send("Unauthorized");
         }
        // jwt.verify(token, process.env.TOKEN_SECRET, {}, (err, user) => {
-        jwt.verify(token, env.TOKEN_SECRET, {}, (err, user) => {
+        jwt.verify(token, process.env.TOKEN_SECRET, {}, (err, user) => {
              if (err) {
                 console.log(err);
                 return res.status(StatusCodes.FORBIDDEN).send("Invalid user");
